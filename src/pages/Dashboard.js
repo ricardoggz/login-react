047fb3e9c74c1dfa8e0a2bea81d8a13f68cbc057
ricardoggz <ricardo_ggzicm@outlook.com>
2023-06-01 22:01:43 -0600

    [add]: user validation

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,13 @@
 //Acceso a los usuarios autenticados
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 export const Dashboard = ()=>{
+    const user = JSON.parse(localStorage.getItem('user'))
+    const navigate = useNavigate()
+    useEffect(()=>{
+        if(!user) navigate('/')
+    },[])
     return (
-        <h1>Dashboard</h1>
+        <h1>Bienvenido {!user ? null : user.email}</h1>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 //Login
-import { useState } from "react"
+import { useState, useContext } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
-
+import { UserContext } from "../contexts/UserContext"
 export const Home = ()=>{
     //Cambio de estado para manejar los inputs
     const [input, setInput ] = useState(null)
     const navigate = useNavigate()
+    const { saveUser } = useContext(UserContext)
     const onChange = (evt)=> setInput({
         ...input,
         [evt.target.name] : evt.target.value
@@ -15,6 +16,7 @@ export const Home = ()=>{
         evt.preventDefault()
         const response = await axios.post('http://localhost:3030/login', input)
         if(!response.data.isAuth) return alert(response.data.message)
+        saveUser(input)
         return navigate('/dashboard')
     }
     return (
